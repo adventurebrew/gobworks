3149bebc084e95ad176ef1e802792dd83688421a
BLooperZ <blooperz@users.noreply.github.com>
2023-06-12 23:15:41 +0300

    batch inject graphics

--- gob2_heb/make.bat
+++ gob2_heb/make.bat
@@ -1,8 +1,5 @@
 python -m boozook.text orig -r -i *.ISR -i *.TOT -i *.CAT || echo ERROR && exit /b
 python -m boozook.font patch -r || echo ERROR && exit /b
-python -m boozook.archive -e patch || echo ERROR && exit /b
-python -m boozook.moved.insert_ext .\extracted\INTRO.STK\INTRO0.TOT 2 .\graphics\title.png || echo ERROR && exit /b
-move /y .\TRY.TOT .\extracted\INTRO.STK\INTRO0.TOT || echo ERROR && exit /b
-python -m boozook.archive -c patch || echo ERROR && exit /b
+python -m boozook.graphics patch -r || echo ERROR && exit /b
 
 ..\utils\md5deep64 -bz patch\INTRO.STK > hash.txt
