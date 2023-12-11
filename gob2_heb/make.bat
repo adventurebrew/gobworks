c208dd3229feff16438882030c8a3318fff2666c
BLooperZ <blooperz@users.noreply.github.com>
2023-12-11 19:04:49 +0200

    upgrade boozook version

--- gob2_heb/make.bat
+++ gob2_heb/make.bat
@@ -1,5 +1,6 @@
-python -m boozook.text orig -r -i *.ISR -i *.TOT -i *.CAT || echo ERROR && exit /b
-python -m boozook.font patch -r || echo ERROR && exit /b
-python -m boozook.graphics patch -r || echo ERROR && exit /b
+python -m boozook.runner orig ^
+    --text -i *.ISR -i *.TOT -i *.CAT ^
+    --font ^
+    --graphics -r || echo ERROR && exit /b
 
 ..\utils\md5deep64 -bz patch\INTRO.STK > hash.txt
